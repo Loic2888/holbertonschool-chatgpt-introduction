09375ec3f42218c9867e462c97f36ee667283ded
Loic2888 <12188@holbertonstudents.com>
2026-01-12 11:56:51 +0100

    fix: print just arg, and not arg[0]

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-print(f"Nombre d'arguments : {len(sys.argv)}")
-print("Arguments :")
-for i in range(len(sys.argv)):
-	print(f"  sys.argv[{i}] = '{sys.argv[i]}'")
+for arg in sys.argv[1:]:  # Ignore argv[0], affiche seulement les arguments
+	print(arg)
